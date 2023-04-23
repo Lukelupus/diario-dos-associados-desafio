8ba81837360857cfbab91f2bca647659b4aafca0
Lucas Boti <luke.boti.lorken@gmail.com>
2023-04-23 13:35:17 -0300

    corpo artigo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import Publicidade from './Components/Publicidade';
-import Titulo from './Components/Titulo';
-import Colunista from './Components/Colunista';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Publicidade from "./Components/Publicidade";
+import Titulo from "./Components/Titulo";
+import Colunista from "./Components/Colunista";
+import Artigo from "./Components/Artigo";
 
 function App() {
   return (
     <div className="App">
-     <Navbar />
-     <Publicidade src="imagens/Bitmap.png" alt="Estado de Minas Ad" href="https://www.hidglobal.com/omni-id" />
-     <Titulo />
-     <Colunista author="imagens/tamaki-ryushi-mobile.png" name='Tamaki Ryushi'/>
+      <Navbar />
+      <Publicidade
+        src="imagens/Bitmap.png"
+        alt="Estado de Minas Ad"
+        href="https://www.hidglobal.com/omni-id"
+      />
+      <Titulo />
+      <Colunista
+        author="imagens/tamaki-ryushi-mobile.png"
+        name="Tamaki Ryushi"
+      />
+      <Artigo img="imagens/elenco-bacurau-mobile.png" />
     </div>
   );
 }
